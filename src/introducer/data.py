39986dffa5f75ed780c281b2a2fae48071bd21e4
Kilian Lieret <kilian.lieret@posteo.de>
2022-08-04 10:58:23 -0400

    🍻 Before abandoning algorithm

--- src/introducer/data.py
+++ src/introducer/data.py
@@ -2,18 +2,26 @@ import pandas as pd
 
 
 class People:
+    allowed_preference = [2, 3]
+
     def __init__(self, df: pd.DataFrame):
-        self._df = df
+        self._check_df(df)
+        self.df = df
+
+    def _check_df(self, df: pd.DataFrame):
+        assert df.columns.tolist() == ["name", "email", "slack", "preference", "fixed"]
+        assert df.preference.isin(self.allowed_preference).all()
+        assert df.fixed.isin([True, False]).all()
 
     def __len__(self):
-        return len(self._df)
+        return len(self.df)
 
     def n_preference(self, pref):
-        assert pref in [1, 2]
-        return len(self._df.query(f"preference == {pref}"))
+        assert pref in self.allowed_preference
+        return len(self.df.query(f"preference == {pref}"))
 
     def n_preference_fixed(self, pref=None):
-        assert pref in [None, 1, 2]
+        assert pref in [None, *self.allowed_preference]
         if pref is None:
-            return len(self._df.query("fixed == True"))
-        return len(self._df.query(f"preference == {pref} & fixed == True"))
+            return len(self.df.query("fixed == True"))
+        return len(self.df.query(f"preference == {pref} & fixed == True"))
--- src/introducer/matcher.py
+++ src/introducer/matcher.py
@@ -1,21 +1,12 @@
 from dataclasses import dataclass
 
 from introducer.data import People
+from introducer.util.log import get_logger
 
 
-@dataclass
-class SolutionCount:
-    n_two: int
-    n_three: int
-    n_move_two_to_three: int
-    n_move_three_to_two: int
-    n_remove_three_fixed: int
-    n_remove_two_fixed: int
-
-
-def _find_solution(
+def _find_solution_count(
     n_two: int, n_three: int, n_two_fixed=0, n_three_fixed=0
-) -> tuple[int, int]:
+) -> tuple[int, int, int, int]:
     """
     Find a solution to the problem of matching people to groups without removing anyone.
 
@@ -34,7 +25,7 @@ def _find_solution(
             real_n_three = n_three + move_two_to_three - move_three_to_two
             assert real_n_two + real_n_three == n_two + n_three
             if real_n_two % 2 == 0 and real_n_three % 3 == 0:
-                return real_n_two, real_n_three
+                return real_n_two, real_n_three, move_three_to_two, move_two_to_three
     raise ValueError("No solution found")
 
 
@@ -47,28 +38,107 @@ def _remove_to_pair(n_remove: int) -> tuple[int, int]:
     return s
 
 
-def find_solution(people: People) -> SolutionCount:
+@dataclass
+class SolutionCount:
+    n_two: int
+    n_three: int
+    n_move_two_to_three: int
+    n_move_three_to_two: int
+    n_remove_three_fixed: int
+    n_remove_two_fixed: int
+
+    def __post_init__(self):
+        for v in self.__dataclass_fields__:
+            assert getattr(self, v) >= 0, v
+
+
+def find_solution_count(people: People) -> SolutionCount:
     for n_remove in range(people.n_preference_fixed()):
         remove_three, remove_two = _remove_to_pair(n_remove)
         try:
-            n_two, n_three = _find_solution(
+            n_two, n_three, move_three_to_two, move_two_to_three = _find_solution_count(
                 people.n_preference(2) - remove_two,
                 people.n_preference(3) - remove_three,
                 people.n_preference_fixed(2) - remove_two,
-                people.n_preference_fixed(3) - remove_two,
+                people.n_preference_fixed(3) - remove_three,
             )
         except ValueError:
             continue
         else:
+            if n_remove:
+                get_logger().warning(f"Had to remove {n_remove} people")
             return SolutionCount(
                 n_two,
                 n_three,
-                n_move_three_to_two=n_two - people.n_preference(2),
-                n_move_two_to_three=n_three - people.n_preference(3),
+                n_move_three_to_two=move_three_to_two,
+                n_move_two_to_three=move_two_to_three,
                 n_remove_two_fixed=remove_two,
                 n_remove_three_fixed=remove_three,
             )
     raise ValueError("No solution found")
+
+
+@dataclass
+class Solution:
+    two: list[int]
+    three: list[int]
+    removed: list[int]
+
+    def __post_init__(self):
+        assert len(self.two) % 2 == 0
+        assert len(self.three) % 3 == 0
+
+
+def _find_solution(people: People, sc: SolutionCount) -> Solution:
+    index = people.df.index
+    assert len(set(index)) == len(index)
+    to_be_removed = (
+        people.df.query("(preference == 2) & (fixed == True)")
+        .sample(n=sc.n_remove_two_fixed)
+        .index.to_list()
+    )
+    to_be_removed += (
+        people.df.query("(preference == 3) & (fixed == True)")
+        .sample(n=sc.n_remove_three_fixed)
+        .index.to_list()
+    )
+    remaining = set(index) - set(to_be_removed)
+    pool_for_two = (
+        people.df.loc[remaining]
+        .query("(preference == 3) & (fixed == False)")
+        .sample(n=sc.n_move_three_to_two)
+        .index.to_list()
+    )
+    pool_for_two += (
+        people.df.loc[remaining]
+        .query("preference == 2")
+        .sample(n=sc.n_two - sc.n_move_three_to_two)
+        .index.to_list()
+    )
+    remaining = set(remaining) - set(pool_for_two)
+    pool_for_three = (
+        people.df.loc[remaining]
+        .query("(preference == 2) & (fixed == False)")
+        .sample(n=sc.n_move_two_to_three)
+        .index.to_list()
+    )
+    pool_for_three += (
+        people.df.loc[remaining]
+        .query("preference == 3")
+        .sample(n=sc.n_three - sc.n_move_two_to_three)
+        .index.to_list()
+    )
+    remaining = set(remaining) - set(pool_for_three)
+    assert not remaining
+    return Solution(
+        two=pool_for_two,
+        three=pool_for_three,
+        removed=to_be_removed,
+    )
+
+
+def find_solution(people: People) -> Solution:
+    return _find_solution(people, find_solution_count(people))
 
 
 # class Partitioner:
