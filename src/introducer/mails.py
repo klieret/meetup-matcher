a626badef8f87ab83777efc5060056827259df8b
Kilian Lieret <kilian.lieret@posteo.de>
2022-08-06 09:42:29 -0400

    ♻️ Extract methods; return iterator

--- src/introducer/mails.py
+++ src/introducer/mails.py
@@ -1,5 +1,6 @@
 import importlib.resources
 from dataclasses import dataclass
+from typing import Iterator
 
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
@@ -15,39 +16,32 @@ class Email:
 
 
 class EmailGenerator:
-    def _generate_remove_email(self, row: pd.Series) -> Email:
+    def __init__(self):
+        template_path = importlib.resources.path(__package__, "templates")
+        self.environment = Environment(loader=FileSystemLoader(template_path))
+
+    def _generate_remove_email(self, person: pd.Series) -> Email:
+        template = self.environment.get_template("removed.txt.jinja")
         return Email(
-            to=[row.email],
-            subject="Postdoc tea-time pairing",
-            content="",
+            to=[person.email],
+            subject="Tea-time pairing",
+            content=template.render(name=person["name"]),
+        )
+
+    def _generate_pair_email(self, group: pd.DataFrame) -> Email:
+        template = self.environment.get_template("matched.txt.jinja")
+        return Email(
+            to=group.email.tolist(),
+            subject="Tea-time pairing",
+            content=template.render(names=group.name.to_list(), people=group),
         )
 
     def generate_emails(
         self, people: People, partitions: list[set[int]], remove: set[int]
-    ) -> list[Email]:
-        template_path = importlib.resources.path(__package__, "templates")
-        environment = Environment(loader=FileSystemLoader(template_path))
-
-        emails = []
-
-        r_template = environment.get_template("removed.txt.jinja")
+    ) -> Iterator[Email]:
         for r in remove:
             person = people.df.loc[r]
-            emails.append(
-                Email(
-                    to=[person.email],
-                    subject="Tea-time pairing",
-                    content=r_template.render(name=person["name"]),
-                )
-            )
-        m_template = environment.get_template("matched.txt.jinja")
+            yield self._generate_remove_email(person)
         for partition in partitions:
             group = people.df.loc[list(partition)]
-            emails.append(
-                Email(
-                    to=group.email.tolist(),
-                    subject="Tea-time pairing",
-                    content=m_template.render(names=group.name.to_list(), people=group),
-                )
-            )
-        return emails
+            yield self._generate_pair_email(group)
