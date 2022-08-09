9c7a3cdb93a360a7b43872448500dab4893165d2
Kilian Lieret <kilian.lieret@posteo.de>
2022-08-09 14:28:38 -0400

    ✅ Add more tests pairs

--- src/meetupmatcher/test_integration.py
+++ src/meetupmatcher/test_integration.py
@@ -1,10 +1,27 @@
+from pathlib import Path
+
+import pytest
+
 from meetupmatcher.main import main
 from meetupmatcher.util.compat_resource import resources
 
 
-def test_integration(capsys):
-    with resources.path("meetupmatcher.test_data", "test.csv") as csv_path:
-        main(["--dry-run", "--seed", "0", str(csv_path)])
+def get_test_pairs() -> list[tuple[Path, Path]]:
+    test_files_dir = Path(resources.files("meetupmatcher.test_data"))  # type: ignore
+    test_files: list[Path] = sorted(
+        f for f in test_files_dir.iterdir() if f.is_file() and f.name.endswith(".csv")
+    )
+    expect_files: list[Path] = sorted(
+        f
+        for f in test_files_dir.iterdir()
+        if f.is_file() and f.name.endswith(".txt") and f.name.startswith("out_")
+    )
+    return list(zip(test_files, expect_files))
+
+
+@pytest.mark.parametrize("inpt,outpt", get_test_pairs())
+def test_integration(inpt: Path, outpt: Path, capsys):
+    main(["--dry-run", "--seed", "0", str(inpt)])
     captured = capsys.readouterr()
-    expected = resources.read_text("meetupmatcher.test_data", "out.txt")
+    expected = outpt.read_text()
     assert captured.out == expected
