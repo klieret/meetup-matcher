7e762e3c0388a88f06471ed4cd6420851cba031e
Kilian Lieret <kilian.lieret@posteo.de>
2022-08-11 10:45:02 -0400

    🚸 Log seed

--- src/meetupmatcher/pseudorandom.py
+++ src/meetupmatcher/pseudorandom.py
@@ -4,6 +4,8 @@ import time
 
 import numpy as np
 
+from meetupmatcher.util.log import logger
+
 
 def get_weeks_since_epoch(timestamp: float = None) -> int:
     """
@@ -19,7 +21,9 @@ def get_random_seed(timestamp: float = None) -> int:
 
 
 def get_rng(timestamp: float = None) -> np.random.Generator:
-    return np.random.default_rng(get_random_seed(timestamp=timestamp))
+    seed = get_random_seed(timestamp=timestamp)
+    logger.info(f"Seed set to {seed}")
+    return np.random.default_rng(seed)
 
 
 def get_rng_from_option(option: str) -> np.random.Generator:
