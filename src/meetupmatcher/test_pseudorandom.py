72b626728e77d0fc92156c13bc5751db583dd0e0
Kilian Lieret <kilian.lieret@posteo.de>
2022-08-19 19:17:15 -0400

    ♻️ Refactor & test date specification

--- src/meetupmatcher/test_pseudorandom.py
+++ src/meetupmatcher/test_pseudorandom.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from datetime import datetime
 
-from meetupmatcher.pseudorandom import get_weeks_since_epoch
+from meetupmatcher.pseudorandom import get_seed_from_option, get_weeks_since_epoch
 
 
 def test_get_weeks_since_epoch():
@@ -12,3 +12,15 @@ def test_get_weeks_since_epoch():
         )
         == 2744
     )
+
+
+def test_get_seed_from_option():
+    assert get_seed_from_option("Aug 12 2022") == 2745
+
+
+def test_get_seed_from_explicit():
+    assert get_seed_from_option("2745") == 2745
+
+
+def test_get_seed_from_nothing():
+    assert get_seed_from_option("week") >= 2745
