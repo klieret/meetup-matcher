6acb1ac5b2c1a8f02f7f904412811d820843cade
Kilian Lieret <kilian.lieret@posteo.de>
2023-01-14 12:33:30 -0500

    ✅ Add Optional type hints

--- src/meetupmatcher/pseudorandom.py
+++ src/meetupmatcher/pseudorandom.py
@@ -8,7 +8,7 @@ import numpy as np
 from meetupmatcher.util.log import logger
 
 
-def get_weeks_since_epoch(timestamp: float = None) -> int:
+def get_weeks_since_epoch(timestamp: float | None = None) -> int:
     """
     Returns the number of weeks since epoch.
     """
@@ -22,11 +22,16 @@ def get_weeks_since_epoch(timestamp: float = None) -> int:
     return n_weeks
 
 
-def get_random_seed_from_timestamp(timestamp: float = None) -> int:
+def get_random_seed_from_timestamp(timestamp: float | None = None) -> int:
+    """Get random seed from timestamp. If timestamp is None, use current time."""
     return get_weeks_since_epoch(timestamp)
 
 
 def get_seed_from_option(option: str) -> int:
+    """Get seed from string input specified in command line. Supported options:
+    `week` (current week), a number (directly taken as seed), or
+    anything that can be parsed by `dateutil.parser.parse`.
+    """
     if option == "week":
         logger.debug("Getting seed from week number")
         return get_random_seed_from_timestamp()
@@ -48,6 +53,9 @@ def get_rng_from_seed(seed: float) -> np.random.Generator:
 
 
 def get_rng_from_option(option: str) -> np.random.Generator:
+    """Get random number generator from string input specified in
+    command line. See `get_seed_from_option` for supported options.
+    """
     logger.debug("Seed option is %s", option)
     seed = get_seed_from_option(option)
     logger.info("Seed is %s", seed)
